9a4ffef1e1fb45b1cf6ad54e5213be900a1dc37a
Orisabiyi <orisabiyidavid@gmail.com>
2024-06-21 18:17:27 +0100

    created a component for logic, calculated the center position, bottom placement for button of the screen and update the position placement

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 import customtkinter
 
+from logic import *
+
+# app instance
 app = customtkinter.CTk()
+
+# system screen size
+width = app.winfo_screenwidth()
+height = app.winfo_screenheight()
+custom_size = str(width) + 'x' + str(height)
+
+app.geometry(custom_size)
+app.title('Lip Sync')
+
+screen_center_horizontal = width / 2.2
+screen_bottom_place = height - 200
+
+button = customtkinter.CTkButton(master=app, text='Generate Sync Video', command=generate)
+button.place(x=screen_center_horizontal, y=screen_bottom_place)
+
 
 app.mainloop()
