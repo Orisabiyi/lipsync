0c5fd21445c43b580f1c43e682321405e9aa09c3
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-01 10:53:50 +0100

    refactor: moved the get_settings function to a new file

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from tkinter import Scrollbar, VERTICAL
 import customtkinter
-
 from PIL import Image, ImageTk
 import cv2
-from validate_values import *
+
+from settingvalue import get_settings
 from color import *
 
 
@@ -265,7 +265,7 @@ class App(customtkinter.CTk):
       self.open_audio = customtkinter.CTkButton(self.upload, text='Click to upload image or video', width=self.width / 2, fg_color=PRIMARY_COLOR, hover_color=PRIMARY_COLOR, corner_radius=10, command=self.open_audio_dialog)
       self.open_audio.pack(anchor='w', padx=(20, 20), pady=(20, 0), ipady=10)
 
-      self.submit_btn = customtkinter.CTkButton(self.upload, text='Sync Video', width=self.width / 2, fg_color=PRIMARY_COLOR, hover_color=PRIMARY_COLOR, corner_radius=10, command=self.get_settings)
+      self.submit_btn = customtkinter.CTkButton(self.upload, text='Sync Video', width=self.width / 2, fg_color=PRIMARY_COLOR, hover_color=PRIMARY_COLOR, corner_radius=10, command=lambda : get_settings(self))
       self.submit_btn.pack(anchor='w', padx=(20, 20), pady=(20, 0), ipady=10)
       
 
@@ -321,42 +321,6 @@ class App(customtkinter.CTk):
             self.preview_label.configure(image=img_preview, text='')
             self.preview_label.image = img_preview  # Keep a reference
         cap.release()
-  
-  def get_settings(self):
-      # validate values first before accepting and processing
-      validate_values(self=self)
-
-      version = self.model_select.get()
-      quality = self.model_quality_select.get()
-      static = self.check_static.get()
-      rotate = self.check_rotate.get()
-      smooth = self.check_smooth.get()
-      super_resolution =self.check_res.get()
-
-      padding = {'top': self.pad_1, 'bottom': self.pad_2, 'left': self.pad_3, 'right': self.pad_4}
-
-      box = {'top': self.bound_1, 'bottom': self.bound_2, 'left': self.bound_3, 'right': self.bound_4}
-
-      crop = {'top': self.crop_1, 'bottom': self.crop_2, 'left': self.crop_3, 'right': self.crop_4}
-
-      if (self.face_entry_3.get() == 'True'): mouth_tracking = True 
-      else: mouth_tracking = False
-
-      if (self.face_entry_4.get() == 'True'): debug_mask = True 
-      else: debug_mask = False
-
-      face_mask = {'size': self.face_1, 'feathering': self.face_2, 'mouth_tracking': mouth_tracking, 'debug_mask': debug_mask}
-
-      upscaler = self.upscaler.get()
-
-      frames_per_second = self.frames_per_second.get()
-
-      face = self.file_name_label.cget('text').replace('Selected: ', '')
-      audio = self.file_audio_label.cget('text')
-
-      print(version, quality, static, rotate, smooth, super_resolution, padding, box, crop, upscaler, frames_per_second, face, audio, face_mask)
-
-      # self.invoke(version=version, quality=quality, static=static, rotate=rotate, smooth=smooth, super_resolution=super_resolution, padding=padding, box=box, crop=crop, upscaler=upscaler, frames_per_second=frames_per_second, face=face, audio=audio)
 
   def invoke(self):
       pass
---
+++ settingvalue.py
@@ -0,0 +1,35 @@
+from validate_values import validate_values
+
+def get_settings(self):
+      # validate values first before accepting and processing
+      validate_values(self=self)
+
+      version = self.model_select.get()
+      quality = self.model_quality_select.get()
+      static = self.check_static.get()
+      rotate = self.check_rotate.get()
+      smooth = self.check_smooth.get()
+      super_resolution =self.check_res.get()
+
+      padding = {'top': self.pad_1, 'bottom': self.pad_2, 'left': self.pad_3, 'right': self.pad_4}
+
+      box = {'top': self.bound_1, 'bottom': self.bound_2, 'left': self.bound_3, 'right': self.bound_4}
+
+      crop = {'top': self.crop_1, 'bottom': self.crop_2, 'left': self.crop_3, 'right': self.crop_4}
+
+      if (self.face_entry_3.get() == 'True'): mouth_tracking = True 
+      else: mouth_tracking = False
+
+      if (self.face_entry_4.get() == 'True'): debug_mask = True 
+      else: debug_mask = False
+
+      face_mask = {'size': self.face_1, 'feathering': self.face_2, 'mouth_tracking': mouth_tracking, 'debug_mask': debug_mask}
+
+      upscaler = self.upscaler.get()
+
+      frames_per_second = self.frames_per_second.get()
+
+      face = self.file_name_label.cget('text').replace('Selected: ', '')
+      audio = self.file_audio_label.cget('text')
+
+      print(version, quality, static, rotate, smooth, super_resolution, padding, box, crop, upscaler, frames_per_second, face, audio, face_mask)
